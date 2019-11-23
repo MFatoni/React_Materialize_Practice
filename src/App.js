12b0ace1b71e1523db449d9aa5edd11bfbe70df0
Fatoni <mohfatoni@yahoo.com>
2019-11-23 22:50:29 +0700

    Trying react-materialize

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {
+  Button,
+  Card,
+  CardTitle,
+  Row,
+  Col,
+  TextInput,
+  Carousel
+} from "react-materialize";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Carousel
+          options={{fullWidth: true}}
+          images={[
+            "https://lorempixel.com/800/400/food/1",
+            "https://lorempixel.com/800/400/food/2",
+            "https://lorempixel.com/800/400/food/3",
+            "https://lorempixel.com/800/400/food/4",
+            "https://lorempixel.com/800/400/food/5"
+          ]}
+        ></Carousel>
+        <Row>
+          <Col m="5">
+            <Card
+              className="small"
+              header={
+                <CardTitle image="https://www.incimages.com/uploaded_files/image/970x450/products_364475.jpg">
+                  Taman Bermain
+                </CardTitle>
+              }
+            >
+              Baju
+            </Card>
+            <Button>Sukses</Button>
+          </Col>
+          <Col m="5">
+            <Card
+              className="small"
+              header={
+                <CardTitle image="https://www.incimages.com/uploaded_files/image/970x450/products_364475.jpg">
+                  Taman Bermain
+                </CardTitle>
+              }
+            >
+              Baju
+            </Card>
+          </Col>
+        </Row>
+        <TextInput type="text" label="Nama" />
+        <Row>
+          <TextInput label="Nama Depan" icon="account_circle" />
+          <TextInput type="file" label="File" />
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default App;
